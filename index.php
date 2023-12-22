671326c7eb5efaa5b55ec89fa9d5abce27d59b33
septaindra14 <83174114+septaindra14@users.noreply.github.com>
2023-12-22 12:54:46 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("Controller/ProductController.php");
-
-use Controller\ProductController;
-
-$productController = new ProductController();
-
-echo $productController->getAllProduct();
