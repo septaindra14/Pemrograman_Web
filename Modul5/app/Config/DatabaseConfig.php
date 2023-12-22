9d3158694bbc92959fd5b5b30802479528ba286a
septaindra14 <83174114+septaindra14@users.noreply.github.com>
2023-12-22 12:56:20 +0700

    Add files via upload

---
+++ Modul5/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\Config;
+
+class DatabaseConfig {
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "pemograman_web";
+    public $port = 3306;
+}
+?>
---
+++ Modul5/app/Controller/ProductController.php
@@ -0,0 +1,60 @@
+<?php
+namespace app\Controller;
+
+include "D:/Pelajaran Kuliah/Praktikum/Smt 5/Pem. Web/Modul5/app/Traits/ApiResponseFormatter.php";
+include "D:/Pelajaran Kuliah/Praktikum/Smt 5/Pem. Web/Modul5/app/Models/Product.php";
+
+use app\Models\Product;
+use app\Traits\ApiResponseFormatter;
+
+class ProductController {
+    use ApiResponseFormatter;
+
+    public function index() {
+        $productModel = new Product();
+        $response = $productModel->findAll();
+        return $this->apiResponse(200, "success", $response);
+    }
+
+    public function getById($id) {
+        $productModel = new Product();
+        $response = $productModel->findById($id);
+        return $this->apiResponse(200, "success", $response);
+    }
+
+    public function insert() {
+        $jsonInput = file_get_contents('php://input');
+        $inputData = json_decode($jsonInput, true);
+        if(json_last_error()) {
+            return $this->apiResponse(400, "error invalid input", null);
+        }
+
+        $productModel = new Product();
+        $response = $productModel->create([
+            "product_name" => $inputData['product_name']
+        ]);
+
+        return $this->apiResponse(200, "success", $response);
+    }
+
+    public function update($id) {
+        $jsonInput = file_get_contents('php://input');
+        $inputData = json_decode($jsonInput, true);
+        if(json_last_error()) {
+            return $this->apiResponse(400, "error invalid input", null);
+        }
+
+        $productModel = new Product();
+        $response = $productModel->update(["product_name" => $inputData['product_name']], $id);
+
+        return $this->apiResponse(200, "success", $response);
+    }
+
+    public function delete($id) {
+        $productModel = new Product();
+        $response = $productModel->destroy($id);
+
+        return $this->apiResponse(200, "success", $response);
+    }
+}
+?>
